1668cb789d78adba20be6476188a5801ff628563
ILYA <106765406+D15ND@users.noreply.github.com>
2026-02-10 04:53:53 +0300

    refactor: add linter echo for husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <h1>Base settings</h1>
-  );
+  return <h1>Base settings</h1>;
 }
 
-export default App
+export default App;
