8e45c7c1b0db6cace307187107f09666f79512af
ILYA <106765406+D15ND@users.noreply.github.com>
2026-02-13 23:39:18 +0300

    feat: add basic css styles

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <div className={styles.container}>
+    <div className={styles.header}>
       <h4 className={styles.title}>Title</h4>
       <p className={styles.desc}>Check</p>
     </div>
