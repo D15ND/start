f266a56425ca67cd9dd0b544bb9906aebff15551
ILYA <106765406+D15ND@users.noreply.github.com>
2026-02-13 23:35:33 +0300

    feat: add header & config settings

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <h4 className={styles.title}>Title</h4>
+      <p className={styles.desc}>Check</p>
+    </div>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +13,16 @@ export default defineConfig({
     minify: false,
     sourcemap: true,
     outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@common': path.resolve(__dirname, 'src/common'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@api': path.resolve(__dirname, 'src/api'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+    },
   },
 });
